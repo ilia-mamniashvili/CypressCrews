93bb380eb5df5b98a030235394f0d9ee111e5000
ilia-mamniashvili <93448416+ilia-mamniashvili@users.noreply.github.com>
2025-01-08 00:24:41 +0400

    Update addCrew.js

--- cypress/integration/addCrew.js
+++ cypress/integration/addCrew.js
@@ -9,22 +9,11 @@ describe('Add Crew', () => {
 
         const addCrewElements = new AddCrewElements();
 
-        //cy.CrewPage();
         cy.wait(10000);
         addCrewElements.CrewNavPage();
         cy.wait(5000);
         addCrewElements.CrewsPage(); 
         addCrewElements.CrewAddButton();
         cy.CreateCrew();
-        // cy.get('.shl-form-field-infix > .ng-pristine').type('Test Name');
-        // cy.get('[data-cy="add-crew-responsible-field"] > div.dynamic-select > :nth-child(1) > .custom-select-header > .ng-select-container > .ng-arrow-wrapper').click();
-        // cy.contains('only, one').click();
-        // cy.get('[data-cy="add-crew-area-field"] > div.dynamic-select > :nth-child(1) > .custom-select-header > .ng-select-container > .ng-arrow-wrapper').click();
-        // cy.contains('Direction 1').click();
-        // cy.get('.mat-mdc-text-field-wrapper').click();
-        // cy.contains('Road construction').click();
-        // cy.get('.add-form__box').should('have.css', 'background-color', 'rgb(243, 26, 26)');
-        // cy.get('[data-cy="add-crew-create-field"]').click();
     });
 });
-
