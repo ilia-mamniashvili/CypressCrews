9c0392fa5118678f090024f4585d1ac1d70708bf
ilia-mamniashvili <93448416+ilia-mamniashvili@users.noreply.github.com>
2025-01-10 15:00:15 +0400

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,10 @@
 import 'cypress-real-events/support';
+
 Cypress.Commands.add('SAMlogin', (email, password) => {
-    cy.visit('https://id.sam.dev.syniotec.com/auth?destination=https:%2F%2Fsam.dev.syniotec.com%2Fexception%2Fnot-found');
+    cy.visit('https://sam.dev.syniotec.com/calendar');
     cy.get(':nth-child(1) > .form-control').type(email);
     cy.get('.form-group--password > .form-control').type(password);
     cy.get('.btn').click();
-});
-
-Cypress.Commands.add('CrewPage', () => {
-    cy.wait(10000);
-        addCrewElements.CrewNavPage();
-        cy.wait(5000);
-        addCrewElements.CrewsPage(); 
 });
 
 Cypress.Commands.add('CreateCrew', () => {
@@ -24,6 +18,45 @@ Cypress.Commands.add('CreateCrew', () => {
         cy.get('.add-form__box').should('have.css', 'background-color', 'rgb(243, 26, 26)');
         cy.get('[data-cy="add-crew-create-field"]').click({force: true});
         cy.get('.confirmation__actions > .filled').click();
+});
+
+Cypress.Commands.add('DeleteCrew', () => {
+    cy.get('.crew-item__text').each(($el) => {
+        const crewName = $el.text().trim();
+
+    if (crewName === 'Test Name') {
+        // Perform actions when the crew with name "Test Name" is found
+        cy.log('Crew with name "Test Name" found!');
+        cy.wrap($el).click();
+        cy.get('[data-cy="crew-delete"] > .fas').click();
+        cy.get('.filled').click();
+        return false; // This will stop the iteration
+    }
+    });
+});
+
+Cypress.Commands.add('addCompanyCar', () => {
+    cy.get('.crew-item__text').each(($el) => {
+        const crewName = $el.text().trim();
+
+        if (crewName === 'Test Name') {
+            // Perform actions when the crew with name "Test Name" is found
+            cy.wrap($el).click();
+            cy.get('[data-cy="company-car-edit"] > .fas').click();
+            cy.get('.company-car__add').click();
+
+            // Click on the dropdown to open the options
+        cy.get('.mat-mdc-select-min-line')
+            .invoke('attr', 'aria-expanded', 'true')
+            .click();
+            // Select the "Transporter" option by clicking on the span element
+            cy.contains('#mat-option-0', 'Transporter').realClick();       
+            cy.get(':nth-child(2) > sam-input-control.ng-untouched .ng-untouched').type('as-123-ds');
+            cy.get(':nth-child(3) > sam-input-control.ng-untouched .ng-untouched').type('N132');
+            cy.get('.company-car-modal__footer-submit').click();
+            return false; // This will stop the iteration
+        }
+    });
 });
 
 // ***********************************************
