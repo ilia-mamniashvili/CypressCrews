e01967e2c3fec4ab88ccf06f5ec2e55155c32537
ilia-mamniashvili <93448416+ilia-mamniashvili@users.noreply.github.com>
2025-01-10 14:59:01 +0400

    Add files via upload

---
+++ cypress/integration/addCrewCar.js
@@ -0,0 +1,18 @@
+import { AddCrewElements } from '../../crewElements/AddCrewElements.js';
+
+describe('Add Crew', () => {
+    it('should add a new crew member', () => {
+        const email = "reg.driver@syniotec.com";
+        const password = "Qwerty1$";
+        cy.SAMlogin(email, password);
+        cy.get('.lang').click();
+
+        const addCrewElements = new AddCrewElements();
+
+        cy.wait(5000);
+        addCrewElements.CrewsPage(); 
+
+        cy.addCompanyCar();
+        
+    });
+});
