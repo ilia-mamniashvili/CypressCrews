97109dda78341e20fe6d3e9334fd192bc2078546
ilia-mamniashvili <93448416+ilia-mamniashvili@users.noreply.github.com>
2025-01-23 21:26:36 +0400

    Delete cypress directory

--- cypress/crewElements/AddCrewElements.js
+++
@@ -1,9 +0,0 @@
-export class AddCrewElements {
-
-    CrewNavPage = () => cy.get(':nth-child(3) > .nav-item').realHover();
-
-    //CrewsPage = () => cy.get('a[href="/kolone"]').click();
-    CrewsPage = () => cy.get(':nth-child(3) > .nav-item').realHover().get('a[href="/kolone"]').click();
-
-    CrewAddButton = () => cy.get('[data-cy="add-crew-button"]').click();
-}
